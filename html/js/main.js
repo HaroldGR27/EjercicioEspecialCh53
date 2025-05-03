2e6d87b5d5966f06da3a9170ab9c17a740f73248
Harold Iván GR <ivan092704@gmail.com>
2025-05-02 21:42:23 -0600

    Se modifica el js y se agregan los productos

--- html/js/main.js
+++ html/js/main.js
@@ -1,46 +1,45 @@
 const btnLoad = document.getElementById("btnLoad");
-const prods = document.getElementById("prods")
+const prods = document.getElementById("prods");
 const api = "https://api.escuelajs.co/api/v1/products";
 
-btnAgregar.addEventListener("click", function (event) {
-    event.preventDefault();
-
+function getData() {
     fetch(api)
-        .then((response) => {
-            console.log(response)
-            response.json().then(() => {
-                llenarCards(res)
+        .then((response) => response.json())
+        .then((res) => {
+                llenarCards(res);
             })
-        })
         .catch((err) => {
-            main.insertAdjacentHTML("beforeend",
-                `<div class="alert alert-danger" role="alert">
+            main.insertAdjacentHTML("beforeend",`
+                <div class="alert alert-danger" role="alert">
                  ${err.message}
             </div>`);
         })
+    }
+
+btnLoad.addEventListener("click", function (event) {
+    event.preventDefault();
+    getData();
 });
 
-function llenarCards(prods) {
+function llenarCards(prod) {
     prods.innerHTML="";
-    let i = 0
-    do{
-        prods.insertAdjacentHTML("beforeend",
-        `
+    for (let i = 0; i < Math.min(9, prod.length); i++) {
+        prods.insertAdjacentHTML("beforeend",`
         <div class="col">
           <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <img src="${prod[i].images[1]}" referrerpolicy="no-referrer" class="card-img-top" width="100%" height="225" alt="${prod[i].title}">
             <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text">${prod[i].title}</p>
+              <p class="card-text">${prod[i].description}</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                   <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
                   <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                 </div>
-                <small class="text-body-secondary">$ price</small>
+                <small class="text-body-secondary">$${prod[i].price}</small>
               </div>
             </div>
           </div>
         </div>`);
-        i++
-    } while (i <= 9);
+    } 
 }
